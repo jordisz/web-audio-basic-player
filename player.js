0384a67268f42e0337e6de581e7738421455427a
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-05-12 17:11:49 +0200

    Play / pause audio

--- player.js
+++ player.js
@@ -1,1 +1,34 @@
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
 
+const audioElement = document.querySelector("audio");
+const track = audioContext.createMediaElementSource(audioElement);
+const gainNode = audioContext.createGain();
+
+track.connect(gainNode).connect(audioContext.destination);
+
+const playButton = document.querySelector("button");
+playButton.addEventListener(
+  "click",
+  function () {
+    if (audioContext.state === "suspended") {
+      audioContext.resume();
+    }
+    if (this.dataset.playing === "false") {
+      audioElement.play();
+      this.dataset.playing = "true";
+    } else if (this.dataset.playing === "true") {
+      audioElement.pause();
+      this.dataset.playing = "false";
+    }
+  },
+  false
+);
+
+audioElement.addEventListener(
+  "ended",
+  () => {
+    playButton.dataset.playing = "false";
+  },
+  false
+);
